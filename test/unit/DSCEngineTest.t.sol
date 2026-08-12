bf419ef41f1c397a0e7faa6ffec045b6dbea925a
caozhengqian <1264419544@qq.com>
2026-08-13 01:25:45 +0800

    add 测试传入0抵押物的报错

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -6,6 +6,7 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -18,11 +19,15 @@ contract DSCEngineTest is Test {
     address public weth;
     address public wbtc;
     uint256 public deployerKey;
+    address public USER = makeAddr("user");
+     uint256 public constant amountCollateral = 10 ether;
+         uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
 
     function setUp() public {
-         deployer = new DeployDSC();
+        deployer = new DeployDSC();
         (dsc, dsce, helperConfig) = deployer.run();
         (ethUsdPriceFeed, btcUsdPriceFeed, weth, wbtc, deployerKey) = helperConfig.activeNetworkConfig();
+        ERC20Mock(weth).mint(USER,STARTING_ERC20_BALANCE);
     }
 
     //////////////////
@@ -34,5 +39,17 @@ contract DSCEngineTest is Test {
         uint256 expectedUsd = 30_000e18;
         uint256 usdValue = dsce.getUsdValue(weth, ethAmount);
         assertEq(usdValue, expectedUsd);
+    }
+
+    ///////////////////////////////////////
+    // depositCollateral Tests //
+    ///////////////////////////////////////
+    function testRevertsIfTransferFromFails() public {
+        vm.startPrank(USER);
+         ERC20Mock(weth).approve(address(dsce), amountCollateral);
+         // 使用selector,测试存入0的报错
+         vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+         dsce.depositCollateral(weth,0);
+         vm.stopPrank();
     }
 }
