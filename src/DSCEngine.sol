d08cf8e4a5c20131c9ca5684479b966e0300c4d1
caozhengqian <1264419544@qq.com>
2026-08-20 20:08:44 +0800

    add 清算

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -32,6 +32,9 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__BreaksHealthFactor(uint256 healthFactorValue);
     error DSCEngine__TransferFailed();
     error DSCEngine__MintFailed();
+    //健康检查OK
+    error DSCEngine__HealthFactorOk();
+    error DSCEngine__HealthFactorNotImproved();//健康没有改善
     ///////////////////
     // State Variables
     ///////////////////
@@ -43,6 +46,7 @@ contract DSCEngine is ReentrancyGuard {
     uint256 private constant LIQUIDATION_THRESHOLD = 50; // This means you need to be 200% over-collateralized
     uint256 private constant LIQUIDATION_PRECISION = 100;
     uint256 private constant MIN_HEALTH_FACTOR = 1e18;
+    uint256 private constant LIQUIDATION_BONUS = 10; //奖金的10%（总数*10）/100 = 总数*10%
     /// @dev Amount of collateral deposited by user
     // [A用户=>[ETH=>100]
     mapping(address user => mapping(address collateralToken => uint256 amount)) private s_collateralDeposited;
@@ -59,7 +63,9 @@ contract DSCEngine is ReentrancyGuard {
     //使用indexed关键字索引，日志中更容易搜索和过滤。
     //对于地址类型的参数，使用indexed可根据特定的地址来查找相关的事件日志。
     event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
-    event CollateralRedeemed(address indexed user, address indexed token, uint256 amount);
+    event CollateralRedeemed(
+        address indexed redeemedFrom, address indexed redeemedTo, address indexed token, uint256 amount
+    );
     ///////////////////
     // Modifiers
     ///////////////////
@@ -148,27 +154,16 @@ contract DSCEngine is ReentrancyGuard {
         moreThanZero(amountCollateral)
         nonReentrant
     {
-        //s_collateralDeposited:抵押物[A用户=>[ETH=>100]
-        s_collateralDeposited[msg.sender][tokenCollateralAddress] -= amountCollateral;
-        emit CollateralRedeemed(msg.sender, tokenCollateralAddress, amountCollateral);
-        // _calculateHealthFactorAfter()
-        bool success = IERC20(tokenCollateralAddress).transfer(msg.sender, amountCollateral);
-        if (!success) {
-            revert DSCEngine__TransferFailed();
-        }
+        _redeemCollateral(tokenCollateralAddress, amountCollateral, msg.sender, msg.sender);
         _revertIfHealthFactorIsBroken(msg.sender);
     }
 
     //第十二步1：燃烧币,并检查抵押物是否健康
     function burnDsc(uint256 amount) public moreThanZero(amount) {
-        s_DSCMinted[msg.sender] -= amount;//[A用户=>100 DSC币]
-        bool success = i_dsc.transferFrom(msg.sender, address(this), amount);
-        if (!success) {
-            revert DSCEngine__TransferFailed();
-        }
-        i_dsc.burn(amount);
+        _burnDsc(amount, msg.sender, msg.sender);
         _revertIfHealthFactorIsBroken(msg.sender);
     }
+
     // If we do start nearing undercollateralization,we need someone to liquidate positions
     // $100 ETH backing $50 DSC
     // $ 20 ETH back $50 DSC ,DSC isn't worth $1!!!
@@ -184,26 +179,67 @@ contract DSCEngine is ReentrancyGuard {
     *   @notice You will get a liquidation bonus for taking the users funds
     *   @
     */
+    // 第十三步0：清算系统
     function liquidate(address collateral, address user, uint256 debtToCover)
         external
         moreThanZero(debtToCover)
         nonReentrant
-    {}
+    {
+        //check health
+        uint256 startingUserHealthFactor = _healthFactor(user);
+        if (startingUserHealthFactor >= MIN_HEALTH_FACTOR) {
+            revert DSCEngine__HealthFactorOk();
+        }
+        // burn DSC
+        // take their collateral
+        // If covering 100 DSC, we need to $100 of collateral
+        uint256 tokenAmountFromDebtCovered = getTokenAmountFromUsd(collateral, debtToCover);
+        //给结算者10%利息
+        // We should implement a feature to liquidate in the event the protocol is insolvent
+        // 我们应实施一项功能，以便在协议无法履行时进行清算
+        // And sweep extra amounts into a treasury并将额外的款项存入国库
+        //0.05ETH * 0.1 = 0.005ETH
+        uint256 bonusCollateral = (tokenAmountFromDebtCovered * LIQUIDATION_BONUS) / LIQUIDATION_PRECISION;
+        // 得到总额（0.05 + 0.005 = 0.055ETH）
+        uint256 totalCollateralToRedeem = tokenAmountFromDebtCovered + bonusCollateral;
+        // 进行redeem赎回
+        _redeemCollateral(collateral, totalCollateralToRedeem, user, msg.sender);
+        // 燃烧DSC，偿还债务的人
+        _burnDsc(debtToCover, user, msg.sender);
+        uint256 endingUserHealthFactor = _healthFactor(user);
+        // This conditional should never hit, but just in case
+        if (endingUserHealthFactor <= startingUserHealthFactor) {
+            revert DSCEngine__HealthFactorNotImproved();
+        }
+        _revertIfHealthFactorIsBroken(msg.sender);
+    }
 
     function getHealthFactor() external {}
 
     ///////////////////
     // Public Functions
     ///////////////////
+    // 第十三步1：清算系统 的USD转为多少ETH
+    function getTokenAmountFromUsd(address token, uint256 usdAmountInWei) public view returns (uint256) {
+        // price of ETH(token)
+        // $/ETH=>ETH?
+        // $2000 /ETH ==> $1000 = 0.5 ETH
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        // $10e18 * 1e18 /($2000e8 * 1e10)
+        uint256 priceUint256 = uint256(price);
+        return ((usdAmountInWei * PRECISION) / (priceUint256 * ADDITIONAL_FEED_PRECISION));
+    }
+
     /*
      * @param amountDscToMint: The amount of DSC you want to mint
      * You can only mint DSC if you have enough collateral
      */
     // 第八步：创造DSC币
     function mintDsc(uint256 amountDscToMint) public moreThanZero(amountDscToMint) nonReentrant {
-        s_DSCMinted[msg.sender] += amountDscToMint;//s_DSCMinted:[A用户=>100 DSC币]
+        s_DSCMinted[msg.sender] += amountDscToMint; //s_DSCMinted:[A用户=>100 DSC币]
 
-        bool minted = i_dsc.mint(msg.sender, amountDscToMint);//创造DSC代币
+        bool minted = i_dsc.mint(msg.sender, amountDscToMint); //创造DSC代币
         if (minted != true) {
             revert DSCEngine__MintFailed();
         }
@@ -216,8 +252,8 @@ contract DSCEngine is ReentrancyGuard {
         //loop through each collateral token,get the amount they have deposited,and map it to
         //the price, to get the USD value
         for (uint256 index = 0; index < s_collateralTokens.length; index++) {
-            address token = s_collateralTokens[index];//[ETH,BTC]
-            uint256 amount = s_collateralDeposited[user][token];//[A用户=>[ETH=>100]
+            address token = s_collateralTokens[index]; //[ETH,BTC]
+            uint256 amount = s_collateralDeposited[user][token]; //[A用户=>[ETH=>100]
             totalCollateralValueInUsd += getUsdValue(token, amount);
         }
         return totalCollateralValueInUsd;
@@ -270,10 +306,32 @@ contract DSCEngine is ReentrancyGuard {
         view
         returns (uint256 totalDescMinted, uint256 collateralValueInUsd)
     {
-        totalDescMinted = s_DSCMinted[user];//s_DSCMinted[A用户=>100 DSC币]
+        totalDescMinted = s_DSCMinted[user]; //s_DSCMinted[A用户=>100 DSC币]
         collateralValueInUsd = getAccountCollateralValue(user);
         // loop through each collateral token, get the amount they have deposited, and convert it to USD value
         // add all the USD values together
         // return the total value in USD
+    }
+
+    function _redeemCollateral(address tokenCollateralAddress, uint256 amountCollateral, address from, address to)
+        private
+    {
+        //s_collateralDeposited:抵押物[A用户=>[ETH=>100]
+        s_collateralDeposited[from][tokenCollateralAddress] -= amountCollateral;
+        emit CollateralRedeemed(from, to, tokenCollateralAddress, amountCollateral);
+        // _calculateHealthFactorAfter()
+        bool success = IERC20(tokenCollateralAddress).transfer(to, amountCollateral);
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
+    }
+
+    function _burnDsc(uint256 amountDscToBurn, address onBehalfOf, address dscFrom) private {
+        s_DSCMinted[onBehalfOf] -= amountDscToBurn; //[A用户=>100 DSC币]
+        bool success = i_dsc.transferFrom(dscFrom, address(this), amountDscToBurn);
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
+        i_dsc.burn(amountDscToBurn);
     }
 }
